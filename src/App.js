3bc2700a69602b33e66725c95ee0992de109d1b3
kabomDev <kabom.y@yahoo.com>
2020-03-02 14:49:31 +0100

    mise en place du state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import{sampleText} from './sampleText'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  //on définit dans state le contenu de sampleText
+  state = {
+    text: sampleText
+  }
+
+  //fonction qui permet la modification du state
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({text})
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea 
+              className='form-control' 
+              row='40' value={this.state.text} 
+              onChange={this.handleChange} >
+  
+            </textarea>
+          </div>
+          <div className="col-sm-6">
+            <h2>Résultats</h2>
+            {sampleText}
+          </div>
+        </div>
+      </div>
+    );
+  } 
 }
 
 export default App;
